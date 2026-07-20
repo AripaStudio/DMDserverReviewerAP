c1cbb5757dec3fab222e23b53ea1f965b8dc0e27
khashayar <aripastudio44@gmail.com>
2026-07-20 21:51:33 +0330

    Update v1.0.0: Fix multiple bugs & improve CLI layout layout

---
+++ DMDserverReviewerAP/CreateUI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DMDserverReviewerAP
+{
+    public static class UserInterfaceManager
+    {
+        public static void Create(string nameProcess, int processId, int index, long ramUsage, double cpuUsage)
+        {
+            int targetLine = 4 + index;
+
+            Console.SetCursorPosition(0, targetLine);
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"{processId,-6}");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("]");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"  {nameProcess,-15}");
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write($"  RAM: {ramUsage,4} MB");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($"  CPU: {cpuUsage,5:F1} %");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("  [");
+            Console.ForegroundColor = ramUsage > 1024 ? ConsoleColor.Red : ConsoleColor.Green;
+            Console.Write(ramUsage > 1024 ? "LIMIT!" : "ACTIVE");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("]");
+
+            Console.Write(new string(' ', 10));
+            Console.ResetColor();
+        }
+
+        public static void DrawHeader(long maxLimit)
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("==================================================================");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"  DMDServer Watchdog Active | Max Limit: {maxLimit} MB");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("==================================================================");
+            Console.ResetColor();
+        }
+
+        public static void DrawFooter(int processCount)
+        {
+            Console.SetCursorPosition(0, 4 + processCount);
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("==================================================================");
+            Console.ResetColor();
+        }
+    }
+}
--- DMDserverReviewerAP/Program.cs
+++ DMDserverReviewerAP/Program.cs
@@ -2,11 +2,14 @@
 
 using System.Diagnostics;
 using System.Timers;
+using DMDserverReviewerAP;
 using Timer = System.Timers.Timer;
 
+HashSet<int> processIdsHashSet = new HashSet<int>();
 long MaxSizeMB = 1024;
 long MaxSizeInWorkingTime = MaxSizeMB + 1024;
 bool DMDserverExists = true;
+
 
 // CPU Usage % = (cpuUsedMs / (totalMsPassed * ProcessorCount)) * 100
 double GetCpuUsage(Process process)
@@ -51,50 +54,95 @@ void ReviewDMD()
 {
 
     var getByName = Process.GetProcessesByName("dmdserver");
+
+    int currentLeft = Console.CursorLeft;
+    int currentTop = Console.CursorTop;
+
     if (getByName.Length == 0)
     {
+        Console.SetCursorPosition(0, 0);
         Console.ForegroundColor = ConsoleColor.DarkCyan;
         Console.WriteLine("dmd server not found.");
         Console.ResetColor();
         DMDserverExists = false;
         return;
     }
+    UserInterfaceManager.DrawHeader(MaxSizeMB);
     DMDserverExists = true;
-    Console.ForegroundColor = ConsoleColor.DarkCyan;
-    Console.WriteLine("dmd server is Run.");
-    Console.ResetColor();
+
+    int index = 0;
     foreach (Process process in getByName)
     {
-        using (process)
-        {
-            if (process.HasExited) continue;
 
+
+        int pid = process.Id;
+        string processName = process.ProcessName;
+
+
+        if (processIdsHashSet.Contains(pid))
+        {
+            CheckMemoryLimit(process, index);
+            index++;
+            continue;
+        }
+
+        processIdsHashSet.Add(pid);
+        try
+        {
+            process.EnableRaisingEvents = true;
+            process.Exited += (sender, eventArgs) =>
+            {
+                processIdsHashSet.Remove(pid);
+                Console.WriteLine($"Process {pid} has exited.");
+                Console.WriteLine($"Process name : {processName} has exited.");
+            };
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"err = {exception.Message}");
+        }
+
+        CheckMemoryLimit(process, index);
+        index++;
+
+
+
+    }
+    UserInterfaceManager.DrawFooter(getByName.Length);
+
+    Console.SetCursorPosition(currentLeft, currentTop);
+
+    void CheckMemoryLimit(Process process, int idx)
+    {
+
+        try
+        {
             long workingSetMB = process.WorkingSet64 / (1024 * 1024);
             var getCpuUsage = GetCpuUsage(process);
+            UserInterfaceManager.Create(process.ProcessName, process.Id, idx, workingSetMB, getCpuUsage);
 
-            if (process.HasExited) continue;
 
             if (workingSetMB > MaxSizeMB)
             {
                 if (getCpuUsage > 10 && workingSetMB < MaxSizeInWorkingTime)
                 {
-
-                    continue;
+                    return;
                 }
+
                 process.Kill();
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Killed due to high memory allocation and low CPU usage.");
-                Console.ResetColor();
+
 
 
             }
         }
+        catch (Exception e)
+        { }
+
     }
 
-
-
-
 }
+
+
 
 void ChangeMaxSize(long input)
 {
@@ -102,11 +150,15 @@ void ChangeMaxSize(long input)
     MaxSizeInWorkingTime = MaxSizeMB + 1024;
 }
 
+Console.Clear();
+UserInterfaceManager.DrawHeader(MaxSizeMB);
+
 Timer timer = new Timer();
 timer.AutoReset = true;
 timer.Interval = 1000;
 timer.Elapsed += TimerOnElapsed;
 timer.Start();
+
 
 void TimerOnElapsed(object? sender, ElapsedEventArgs e)
 {
@@ -123,33 +175,37 @@ void TimerOnElapsed(object? sender, ElapsedEventArgs e)
     timer.Start();
 }
 
-Console.WriteLine("enter a number for change max size limit");
-var ChangeMaxSizeLimit = Console.ReadLine();
-if (ChangeMaxSizeLimit != null)
-{
-    try
-    {
-        if (Convert.ToInt32(ChangeMaxSizeLimit) != 0)
-        {
-            ChangeMaxSize(Convert.ToInt32(ChangeMaxSizeLimit));
-        }
-    }
-    catch (Exception e)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("input not valid.");
-        Console.ResetColor();
-    }
-}
-
-Console.WriteLine($"Max size limit changed to {MaxSizeMB} MB. Watchdog is running...");
+Console.SetCursorPosition(0, 18);
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.WriteLine("==================================================================");
+Console.WriteLine("Command: enter 'exit' to close, or a number to change limit:      ");
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.Write("> ");
+Console.ResetColor();
 while (true)
 {
-    Console.WriteLine("enter a number for change max size limit or for exit (enter 'exit') ");
-
+    Console.SetCursorPosition(2, 20);
     var a = Console.ReadLine();
+
+    Console.SetCursorPosition(2, 20);
+    Console.Write(new string(' ', Console.WindowWidth - 3));
+
     if (a == "exit")
     {
-        return 1;
+        return;
+    }
+
+    if (long.TryParse(a, out long newLimit) && newLimit > 0)
+    {
+        ChangeMaxSize(newLimit);
+        UserInterfaceManager.DrawHeader(MaxSizeMB);
+
+        Console.SetCursorPosition(0, 18);
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("==================================================================");
+        Console.WriteLine("Command: enter 'exit' to close, or a number to change limit:      ");
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.Write("> ");
+        Console.ResetColor();
     }
 }
